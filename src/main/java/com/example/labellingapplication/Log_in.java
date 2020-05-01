8a01ce7e55cc0bfe74c9f1f44a9849015b8b509d
Naod N0742939 <56263577+noadm@users.noreply.github.com>
2020-05-01 15:18:56 +0100

    Add files via upload

---
+++ src/main/java/com/example/labellingapplication/Log_in.java
@@ -0,0 +1,93 @@
+package com.example.labellingapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaScannerConnection;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Log_in extends AppCompatActivity {
+
+    EditText nuserEmail,nPassword;
+    Button nRegister;
+    TextView ngoLogin;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        progressBar = findViewById(R.id.progressBar);
+        nuserEmail = findViewById (R.id.userEmail);
+        nPassword  = findViewById (R.id.Password);
+        nRegister  = findViewById (R.id.Register);
+        ngoLogin   = findViewById (R.id.goLogin);
+        fAuth = FirebaseAuth.getInstance();
+
+        ngoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    String email = nuserEmail.getText().toString().trim();
+                    String password = nPassword.getText().toString().trim();
+
+                    if(TextUtils.isEmpty(email)){
+                        nuserEmail.setError("Email not entered.");
+                        return;
+                    }
+
+                    if(TextUtils.isEmpty(password)){
+                        nPassword.setError("Password not entered.");
+                        return;
+                    }
+
+                    if(password.length() < 9){
+                        nPassword.setError("Password Must 9 Characters or more");
+                        return;
+                    }
+
+                    progressBar.setVisibility(View.VISIBLE);
+
+
+                    fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(Log_in.this,"Log in successful !",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            }else {
+                                Toast.makeText(Log_in.this, "Something went wrong ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+            }
+        });
+
+
+
+        nRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+
+            }
+        });
+    }
+}
